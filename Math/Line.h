4c2f26b36bfe5fdc042e9fe112d96fcf5aff9947
seisner <30507927+SethEisner@users.noreply.github.com>
2020-04-26 19:27:23 -0700

    general cleanup

--- Math/Line.h
+++ Math/Line.h
@@ -80,27 +80,3 @@ private:
 	Vec3 dir;
 	Point3 p;
 };
-
-
-
-
-/*struct Line {
-	Vec3 v; // direction vector v
-	Vec3 m; // moment vector m
-	Line() = default;
-	Line(float vx, float vy, float vz, float mx, float my, float mz) : v(vx, vy, vz), m(mx, my, mz) {}
-	Line(const Vec3& _v, const Vec3& _m) { // can't use initializer list because we need to copy initialize
-		v = _v;
-		m = _m;
-	}
-	Line& operator*=(const Trans4&);
-	friend Line operator*(const Line&, const Trans4&); // transform a plane
-	// dont define an operator ^= because we dont want to chain the operations and that's what that is used for
-	friend Line operator^(const Point3&, const Point3&); // wedge product of two points is a line containing those two points
-	friend Plane operator^(const Line&, const Point3&); // the wedge product of a line and a point is a plane
-	friend Plane operator^(const Point3&, const Line&);
-	friend Vec4 operator^(const Line&, const Plane&); // antiwedge product of a line and a plane is the point of intersection
-	friend Vec4 operator^(const Plane&, const Line&); // antiwedge product of a line and a plane is the point of intersection
-	friend float operator^(const Line&, const Line&); // antiwedge product of a line and a line is their distance
-};*/
-
--- Math/Plane.h
+++ Math/Plane.h
@@ -65,23 +65,3 @@ private:
 	Vec3 n; // normal vector
 	float d; // distance from origin d = -dot(n,q)
 };
-
-/*struct Plane {
-	float x;
-	float y;
-	float z; 
-	float w;
-	Plane() = default;
-	Plane(float _x, float _y, float _z, float _w) : x(_x), y(_y), z(_z), w(_w) {}
-	Plane(const Vec3& n, float d) : x(n.x), y(n.y), z(n.z), w(d) {}
-	const Vec3& get_normal() const;
-	Plane& operator*=(const Trans4&);
-	friend Plane operator*(Plane, const Trans4&); // transform a plane
-	friend Line operator^(const Plane&, const Plane&); // antiwedge product of two planes is their line of intersection
-	friend float operator^(const Point3&, const Plane&); // anti wedge product between a point and a plane is the distance p is from f
-	friend float operator^(const Plane&, const Point3&); // andtiwedge product between a point and a plane is the distance p is from f
-};
-float dot(const Plane&, const Vec3&);
-float dot(const Plane&, const Point3&);
-Trans4 make_reflection(const Plane&);
-*/
